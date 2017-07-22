d35b297a22033e58f5079d59d2ba124f5e066eca
thiemo.belmega@gmail.com <thiemo.belmega@gmail.com>
2017-07-23 01:37:26 +0200

    Add dgrid

--- src/main/webapp/pages/list-stocks-page/ListStocksPage.js
+++ src/main/webapp/pages/list-stocks-page/ListStocksPage.js
@@ -2,11 +2,34 @@ define([
     "dojo/_base/declare",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
+    "dgrid/Grid",
     "dojo/text!stocksim/pages/list-stocks-page/ListStocks.html"
-], function(declare, _WidgetBase, _TemplatedMixin, template) {
+], function(declare, _WidgetBase, _TemplatedMixin, Grid, template) {
 
     return declare([_WidgetBase, _TemplatedMixin], {
-        templateString: template
+        templateString: template,
+
+        startup: function() {
+            var columns = [
+                {
+                    field: 'first',
+                    label: 'First Name'
+                },
+                {
+                    field: 'last',
+                    label: 'Last Name'
+                },
+                {
+                    field: 'age',
+                    label: 'Age'
+                }
+            ];
+
+            let grid = new Grid({
+                columns: columns
+            }, this.gridAttachPoint);
+            grid.startup();
+        }
     });
 
 });
